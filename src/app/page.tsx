4519e1b80a7bd3e1a62f07426a7244bbd552bdb0
kim-limit <rlawogks2468@naver.com>
2023-06-22 17:29:26 +0900

    feat: create toast

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,10 @@ import * as style from './page.style';
 export default function Home() {
   return (
     <div>
-      <style.Container>연준이형 바보</style.Container>
+      <style.Notification>
+        <style.Body>연준이형 바보</style.Body>
+        <style.Progress />
+      </style.Notification>
     </div>
   );
 }
