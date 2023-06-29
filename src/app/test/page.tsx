8de49a1b48c6d8f2dbba6d8927dbba903d042d94
junni1111 <wkszlf98@naver.com>
2023-06-29 19:58:15 +0900

    add : test page for DB test

---
+++ src/app/test/page.tsx
@@ -0,0 +1,26 @@
+import { prisma } from '@/lib/prisma';
+
+export default async function Home() {
+  let users = await prisma.user.findMany();
+  return (
+    <main style={{ maxWidth: 1200, marginInline: 'auto', padding: 20 }}>
+      <div
+        style={{
+          display: 'grid',
+          gridTemplateColumns: '1fr 1fr 1fr 1fr',
+          gap: 20,
+        }}
+      >
+        {users.map((user) => (
+          <div
+            key={user.id}
+            style={{ border: '1px solid #ccc', textAlign: 'center' }}
+          >
+            <h3>{user.name}</h3>
+            <h3>{user.email}</h3>
+          </div>
+        ))}
+      </div>
+    </main>
+  );
+}
